51fc4b6ab8fe299d702817c32723f53b0c0f7217
JLMaksG <g_maks@ukr.net>
2015-04-19 22:31:57 +0300

    Update cosyone.php
    100%

--- catalog/language/ukrainian/common/cosyone.php
+++ catalog/language/ukrainian/common/cosyone.php
@@ -3,14 +3,14 @@
 $_['cosyone_text_mobile_menu'] 	= 'Меню';
 
 // Category pages
-$_['text_category_expire'] 		 = 'Закінчується через:';
+$_['text_category_expire'] 		      = 'Закінчується через:';
 $_['text_category_stock_quantity']  = 'Поспішайте! Залишилось лише %s шт. товару!';
 
 
 // Product page
-$_['text_special_price'] 	 	= 'Акційна ціна:';
-$_['text_old_price']  			= 'Стара ціна:';
-$_['text_you_save']  			= 'Ви економите:';
-$_['text_expire'] 		 		= 'Пропозиція закінчується через:';
+$_['text_special_price'] 	 	  = 'Акційна ціна:';
+$_['text_old_price']  			  = 'Стара ціна:';
+$_['text_you_save']  			    = 'Ви економите:';
+$_['text_expire'] 		 		    = 'Пропозиція закінчується через:';
 $_['text_stock_quantity']  		= 'Поспішайте! Залишилось лише %s шт. товару!';
 $_['text_items_sold']      		= '%s шт. товару вже продано!';
